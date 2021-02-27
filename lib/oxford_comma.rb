9212fcac4a25760158a629b621cb426d2fa69695
imyofather79 <chunsum708@gmail.com>
2021-02-27 08:51:23 -0700

    commit tree 2c304e6ff0f45c1794f14ebe3b88e2f6a97cc195

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+#require 'pry'
 def oxford_comma(array)
+#binding.pry
+array_last = array.pop
+    if array.size == 0
+    array_last
+    
+    elsif array.size == 1
+    array.join(", ") << " and " + array_last
+
+    elsif array.size >= 2
+    array.join(", ") << ", and " + array_last
+
+    else
+    array.join(", ") << ", and " + array_last 
+    end
 
 end
